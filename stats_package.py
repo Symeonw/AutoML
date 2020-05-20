028ea55c802251ec29a450be9899e802de6bf8fa
symeonw <symeonwhite16@gmail.com>
2020-05-20 02:31:33 -0500

    stats research and implementation

--- stats_package.py
+++ stats_package.py
@@ -8,7 +8,7 @@ from itertools import combinations
     #cont_cols
     #cat_cols
     #dropped_cols_stats - [(0, Dropped at CI Test), (1, Dropped at Correlation Test), 
-        #(2, Dropped due at Chi-Squared Test), 
+        #(2, Dropped due at Chi-Squared Test), (3, Dropped at Linear Correlation test), (4, Dropped due to single value in whole column)
 
 class stats_package:
 
@@ -36,7 +36,7 @@ class categorical_target(stats_package):
             ci.append([m-h,m+h])
         return ci
 
-    def continuous_tests_with_cat_target(self):
+    def ci_test(self):
         """Preforms confidence interval test on columns with greater than 100 values"""
         removed_cols = []
         for col in self.cont_cols:
@@ -51,30 +51,30 @@ class categorical_target(stats_package):
                     totals.append(ci[i][1] - ci[i][0])
                 drop_col = np.array(max(maxs)) - np.array(min(mins)) < sum(totals)
                 if drop_col == True:
-                    self.df.drop(columns=[col], inplace=True)
                     self.dropped_cols_stats.update({col:0})
                     removed_cols.append(col)
                 if mins == maxs:
-                    self.df.drop(columns=[col], inplace=True)
                     self.dropped_cols_stats.update({col:0})
                     removed_cols.append(col)
+        self.df.drop(columns=removed_cols, inplace=True)
         [self.cont_cols.remove(item) for item in removed_cols]
 
     
     def clear_over_correlated_columns(self):
         """Checks if two of the continuous columns have over .90 correlation, if so one of them is removed."""
+        removed_cols = []
         corr_list = []
         col_list = list(combinations(self.cont_cols,2))
         for col1,col2 in col_list:
             corr_list.append(self.df[col1].corr(self.df[col2]))
-        drop_list = []
         for corr, cols in zip(corr_list, col_list):
-            if cols[0] in drop_list:
+            if cols[0] in removed_cols:
                 continue
             if corr > .9 :
-                drop_list.append(cols[0])
+                removed_cols.append(cols[0])
                 self.dropped_cols_stats.update({cols[0]:1})
-        self.df.drop(columns = drop_list, inplace=True)
+        self.df.drop(columns = removed_cols, inplace=True)
+        [self.cont_cols.remove(item) for item in removed_cols]
 
     def create_chi_table():
         """Created chi table for scoring"""
@@ -85,18 +85,18 @@ class categorical_target(stats_package):
         return table
 
 
-    def check_chi(self):
+    def chi_test(self):
         removed_cols = []
         for col in self.cat_cols:
-            chi_inp = pd.crosstab(self.df[col], self.df[target])
+            chi_inp = pd.crosstab(self.df[col], self.df[self.target])
             chi2, p, dof, expected = chi2_contingency(chi_inp.values)
             if np.array(i >= 5 for i in expected).all() == False:
+                continue
+            if categorical_target.create_chi_table()[dof-1][6] > chi2 or p > .05 :
                 removed_cols.append(col)
                 self.dropped_cols_stats.update({col:2})
-            if create_chi_table()[dof-1][6] > chi2 or p > .05 :
-                print("These variables are independent, failed to reject H0.")
-            else:
-                print("These variables are dependent, H0 rejected.")
+        [self.cat_cols.remove(item) for item in removed_cols]
+        self.df.drop(columns=removed_cols,inplace=True)
             
 class continuous_target(stats_package):
     def __init__(self, data_file, column_dtypes, user_target_label):
@@ -106,22 +106,102 @@ class continuous_target(stats_package):
         self.cat_cols = list({key:value for (key,value) in column_dtypes.items() if value == "category" if key != user_target_label}.keys())
         self.dropped_cols_stats = {}
 
+    def clear_over_correlated_columns(self):
+        """Checks if two of the continuous columns have over .90 correlation, if so one of them is removed."""
+        removed_cols = []
+        corr_list = []
+        col_list = list(combinations(self.cont_cols,2))
+        for col1,col2 in col_list:
+            corr_list.append(self.df[col1].corr(self.df[col2]))
+        for corr, cols in zip(corr_list, col_list):
+            if cols[0] in removed_cols:
+                continue
+            if corr > .9 :
+                removed_cols.append(cols[0])
+                self.dropped_cols_stats.update({cols[0]:1})
+        self.df.drop(columns = removed_cols, inplace=True)
+        [self.cont_cols.remove(item) for item in removed_cols]
+
+
+    def lin_corr_test(self):
+        """Checks if contious columns have at least +/- .20  correlation with the target column."""
+        removed_cols = []
+        for col in self.cont_cols:
+            if (self.df[col].corr(self.df[self.target]) < .2) | (self.df[col].corr(self.df[self.target]) < -.2):
+                removed_cols.append(col)
+                self.dropped_cols_stats.update({col:3})
+                continue
+            if len(self.df[col].unique()) <= 1:
+                removed_cols.append(col)
+                self.dropped_cols_stats.update({col:4})
+        self.df.drop(columns=removed_cols, inplace=True)
+        [self.cont_cols.remove(item) for item in removed_cols]
+
+
+    def mean_confidence_interval(df:"two column dataframe", confidence=0.95):
+        """Takes in two columns: target(Category) and test column (continous)."""
+        target_len = len(df.iloc[:,1].unique())
+        ci = []
+        for i in range(target_len): #Accessing all unique categories in target, running all groupings of contious variables though test.
+            data = df[df.iloc[:,1] == df.iloc[:,1].unique()[i]].iloc[:,0]
+            a = 1.0 * np.array(data)
+            n = len(a)
+            m, se = np.mean(a), sem(a)
+            h = se * t.ppf((1 + confidence) / 2., n-1)
+            ci.append([m-h,m+h])
+        return ci
+
+    def ci_test(self):
+        """Preforms confidence interval test on columns with greater than 100 values"""
+        removed_cols = []
+        for col in self.cont_cols:
+            if self.df[col].count() >= 100:
+                ci = categorical_target.mean_confidence_interval(self.df[[col, self.target]])
+                maxs = []
+                mins = []
+                totals = []
+                for i in range(len(ci)):
+                    maxs.append(ci[i][1])
+                    mins.append(ci[i][0])
+                    totals.append(ci[i][1] - ci[i][0])
+                drop_col = np.array(max(maxs)) - np.array(min(mins)) < sum(totals)
+                if drop_col == True:
+                    self.dropped_cols_stats.update({col:0})
+                    removed_cols.append(col)
+                if mins == maxs:
+                    self.dropped_cols_stats.update({col:0})
+                    removed_cols.append(col)
+        self.df.drop(columns=removed_cols, inplace=True)
+        [self.cont_cols.remove(item) for item in removed_cols]
 
 
 
 
+    
+
+
+#Continuous Testing
 df = pd.read_csv("test_data/IBM_Data.csv")
 column_dtypes = [0,1,1,0,1,0,1,1,0,0,1,1,0,1,1,1,1,1,0,0,0,1,1,0,1,1,0,1,0,0,1,0,0,0,0]
 test = pop(df,"F647952", column_dtypes, "Attrition")
 test.execute_phase_one()
 test = categorical_target(test.df, test.column_dtypes, test.user_target_label)
-test.continuous_tests_with_cat_target()
-test.dropped_cols_stats
+test.ci_test()
 test.clear_over_correlated_columns()
+test.chi_test()
+test.dropped_cols_stats
 
 
-test.cont_cols
-test.cat_cols
+#Categorical Testing
+df = pd.read_csv("test_data/IBM_Data.csv")
+column_dtypes = [0,1,1,0,1,0,1,1,0,0,1,1,0,1,1,1,1,1,0,0,0,1,1,0,1,1,0,1,0,0,1,0,0,0,0]
+test = pop(df,"F647952", column_dtypes, "Age")
+test.execute_phase_one()
+test = continuous_target(test.df, test.column_dtypes, test.user_target_label)
+test.clear_over_correlated_columns()
+test.lin_corr_test()
+test.dropped_cols_stats
+
 
 
 #For tomorrow:
