20e9d6e262a491b5518356c2ddfd52690fc5bb18
symeonw <symeonwhite16@gmail.com>
2020-05-21 01:31:40 -0500

    data_validation

--- data_validation.py
+++ data_validation.py
@@ -25,12 +25,17 @@ def data_validation(file_path):
 data_validation(file_path)
 
 
-df.StockCode.str.isnumeric().all()
 
-df = pd.read_csv("test_data/IBM_Data.csv")
 
 tested = []
 for col in df.columns:
+    try:
+        if df[col].str.isnumeric().all() == False:
+            tested.append(1)
+            continue
+    except:
+        pass
+
     try:
         if all(x.isdigit() for x in df[col]) == False:
             tested.append(1)
@@ -41,9 +46,63 @@ for col in df.columns:
 
 user_column_input = [0,1,1,0,1,0,1,1,0,0,1,1,0,1,1,1,1,1,0,0,0,1,1,0,1,1,0,1,0,0,1,0,0,0,0]
 
+count = 0
 for x,y,z in zip(tested, user_column_input, df.columns):
     if x == y:
         print("Matched")
     else:
         print("Mislabeled", z, f"Labled {x} while column was {y}")
+        count += 1
+count
+df = pd.read_csv("test_data/IBM_Data.csv")
+df2 = df[["Education", "EnvironmentSatisfaction", "JobInvolvement", "JobLevel", "JobSatisfaction",\
+     "PerformanceRating", "RelationshipSatisfaction", "StockOptionLevel","WorkLifeBalance", "Age", "DailyRate", "MonthlyRate", "DistanceFromHome",\
+         "YearsWithCurrManager",  "YearsSinceLastPromotion", "YearsInCurrentRole", "YearsAtCompany", "TrainingTimesLastYear"]]
+
+
+likely_cat = {}
+for var in df2.columns:
+    likely_cat[var] = 1.*df[var].nunique()/df[var].count() < 0.01 #or some other threshold
+likely_cat
+
+
+#_------------------------------------------------------------------------------------------------------
+
+df = pd.read_csv("test_data/house_data.csv")
+
+
+user_column_input = [0,1,1,0,1,0,1,1,0,0,1,1,0,1,1,1,1,1,0,0,0,1,1,0,1,1,0,1,0,0,1,0,0,0,0]
+
+count = 0
+for x,y,z in zip(tested, user_column_input, df.columns):
+    if x == y:
+        print("Matched")
+    else:
+        print("Mislabeled", z, f"Labled {x} while column was {y}")
+        count += 1
+
+
+tested = []
+for col in df.columns:
+    try:
+        if df[col].str.isnumeric().all() == False:
+            tested.append(1)
+            continue
+    except:
+        pass
+
+    try:
+        if all(x.isdigit() for x in df[col]) == False:
+            tested.append(1)
+    except:
+        if df[col].dtype == float:
+            tested.append(0)
+            continue
+        values = df[col].unique()
+        unique_range = [i for i in range(min(values), max(values)+1)]
+        if (sorted(df[col].unique()) == unique_range) & (min(unique_range) == 1) | (min(unique_range) == 0):
+            print("YAY! ", col, "IS CATEGORICAL")
+            tested.append(1)
+        else:
+            tested.append(0)
 
