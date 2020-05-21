3c6877db925fda381020b159fe1b9b9dd3c58c44
symeonw <symeonwhite16@gmail.com>
2020-05-21 02:12:19 -0500

    data_validation

--- data_validation.py
+++ data_validation.py
@@ -68,10 +68,42 @@ likely_cat
 
 #_------------------------------------------------------------------------------------------------------
 
-df = pd.read_csv("test_data/house_data.csv")
+df = pd.read_csv("test_data/winequality-red.csv")
 
 
-user_column_input = [0,1,1,0,1,0,1,1,0,0,1,1,0,1,1,1,1,1,0,0,0,1,1,0,1,1,0,1,0,0,1,0,0,0,0]
+
+
+tested = [] 
+for col in df.columns:
+    try:
+        if df[col].str.isnumeric().all() == False:
+            tested.append(1)
+            continue
+        #Captures all data with letters
+    except:
+        pass
+
+    try:
+        if all(x.isdigit() for x in df[col]) == False:
+            tested.append(1)
+        #Captures all strings with only numbers
+    except:
+        if df[col].dtype == float:
+            tested.append(0)
+            continue
+        #Captures all  floats
+        values = df[col].unique()
+        unique_range = [i for i in range(min(values), max(values)+1)]
+        if (sorted(df[col].unique()) == unique_range):
+            if (min(unique_range) == 1) | (min(unique_range) == 0):
+                tested.append(1)
+        #Captures all columns who's min value begins with 1 or 0 and all values are present from min() to max() in their unique values.
+        else:
+            tested.append(0)
+df
+print(tested)
+
+user_column_input = [0,]
 
 count = 0
 for x,y,z in zip(tested, user_column_input, df.columns):
@@ -80,29 +112,17 @@ for x,y,z in zip(tested, user_column_input, df.columns):
     else:
         print("Mislabeled", z, f"Labled {x} while column was {y}")
         count += 1
+count
+
+df.PoolArea.unique()
 
 
-tested = []
-for col in df.columns:
-    try:
-        if df[col].str.isnumeric().all() == False:
-            tested.append(1)
-            continue
-    except:
-        pass
+values = df["PoolArea"].unique()
+unique_range = [i for i in range(min(values), max(values)+1)]
+if (sorted(df["PoolArea"].unique()) == unique_range):
+    if (min(unique_range) == 1) | (min(unique_range) == 0):
+        print("YAY! ", "PoolArea", "IS CATEGORICAL")
+else:
+    print("BOO")
 
-    try:
-        if all(x.isdigit() for x in df[col]) == False:
-            tested.append(1)
-    except:
-        if df[col].dtype == float:
-            tested.append(0)
-            continue
-        values = df[col].unique()
-        unique_range = [i for i in range(min(values), max(values)+1)]
-        if (sorted(df[col].unique()) == unique_range) & (min(unique_range) == 1) | (min(unique_range) == 0):
-            print("YAY! ", col, "IS CATEGORICAL")
-            tested.append(1)
-        else:
-            tested.append(0)
-
+unique_range
