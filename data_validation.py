161afb5f79fce4a43b595154ec1dba293fd409ef
symeonw <symeonwhite16@gmail.com>
2020-05-22 01:01:23 -0500

    data validation research

--- data_validation.py
+++ data_validation.py
@@ -1,128 +1,166 @@
 import pandas as pd
 import os
 
-file_path = "test_data/EC_dataset.csv"
-def data_validation(file_path):
+def data_validation(file_path, sheet_name=None):
+    """Check for file size within limits, checks file extention, labels columns."""
+    if os.stat(file_path).st_size/1000000000 > 5:
+        raise ValueError("File sizes of over 4 Gigabytes are not currently supported.")
     
     if file_path.endswith(".csv"):
-        print("here!")
+        try:
+            df = pd.read_csv(file_path)
+        except:
+            df = pd.read_csv(file_path, encoding="unicode_escape")
 
     elif file_path.endswith(".xlsx"):
-        print("here1!")
+        df = pd.read_excel(file_path, sheet_name=sheet_name)
 
     else: 
         raise ValueError(f"{file_path[file_path.rindex('.'):]} file extentioon is not currently supported.")
 
-    if os.stat(file_path).st_size/1000000000 > 5:
-        raise ValueError("File sizes of over 4 Gigabytes are not currently supported.")
+    labels = [] 
+    for col in df.columns:
+        try:
+            if df[col].str.isnumeric().all() == False:
+                labels.append(1)
+                continue
+            #Captures all data with letters
+        except:
+            pass
 
-    df = pd.read_csv(file_path, encoding="unicode_escape")
+        try:
+            if all(x.isdigit() for x in df[col]) == False:
+                labels.append(1)
+            #Captures all strings with only numbers
+        except:
+            if df[col].dtype == float:
+                labels.append(0)
+                continue
+            #Captures all  floats
+            values = df[col].unique()
+            unique_range = [i for i in range(min(values), max(values)+1)]
+            if (sorted(df[col].unique()) == unique_range):
+                if (min(unique_range) == 1) | (min(unique_range) == 0):
+                    labels.append(1)
+            #Captures all columns who's min value begins with 1 or 0 and all values are present from min() to max() in their unique values.
+                else:
+                    labels.append(0)
+            else:
+                    labels.append(0)
+    return df, labels
 
-
-
-
-
-data_validation(file_path)
-
-
-
-
-tested = []
-for col in df.columns:
-    try:
-        if df[col].str.isnumeric().all() == False:
-            tested.append(1)
-            continue
-    except:
-        pass
-
-    try:
-        if all(x.isdigit() for x in df[col]) == False:
-            tested.append(1)
-    except:
-        tested.append(0)
-
-
-
-user_column_input = [0,1,1,0,1,0,1,1,0,0,1,1,0,1,1,1,1,1,0,0,0,1,1,0,1,1,0,1,0,0,1,0,0,0,0]
-
-count = 0
-for x,y,z in zip(tested, user_column_input, df.columns):
-    if x == y:
-        print("Matched")
-    else:
-        print("Mislabeled", z, f"Labled {x} while column was {y}")
-        count += 1
-count
-df = pd.read_csv("test_data/IBM_Data.csv")
-df2 = df[["Education", "EnvironmentSatisfaction", "JobInvolvement", "JobLevel", "JobSatisfaction",\
-     "PerformanceRating", "RelationshipSatisfaction", "StockOptionLevel","WorkLifeBalance", "Age", "DailyRate", "MonthlyRate", "DistanceFromHome",\
-         "YearsWithCurrManager",  "YearsSinceLastPromotion", "YearsInCurrentRole", "YearsAtCompany", "TrainingTimesLastYear"]]
-
-
-likely_cat = {}
-for var in df2.columns:
-    likely_cat[var] = 1.*df[var].nunique()/df[var].count() < 0.01 #or some other threshold
-likely_cat
-
-
-#_------------------------------------------------------------------------------------------------------
-
-df = pd.read_csv("test_data/winequality-red.csv")
+def data_validation_post_user_input(df, user_input): 
+    
+    for col, ui in zip(df.columns, user_input):
+        """"Turns all continuous columns into number only columns (replaces all non-numerical data with NAN"""
+        if ui == 0:
+            df[col] = pd.to_numeric(df[col], errors="coerce")
+    return df
 
 
 
+# df = pd.read_csv("test_data/IBM_Data.csv")
+# user_column_input = [0,1,1,0,1,0,1,1,0,0,1,1,0,1,1,1,1,1,0,0,0,1,1,0,1,1,0,1,0,0,1,0,0,0,0]
 
-tested = [] 
-for col in df.columns:
-    try:
-        if df[col].str.isnumeric().all() == False:
-            tested.append(1)
-            continue
-        #Captures all data with letters
-    except:
-        pass
+# labels = [] 
 
-    try:
-        if all(x.isdigit() for x in df[col]) == False:
-            tested.append(1)
-        #Captures all strings with only numbers
-    except:
-        if df[col].dtype == float:
-            tested.append(0)
-            continue
-        #Captures all  floats
-        values = df[col].unique()
-        unique_range = [i for i in range(min(values), max(values)+1)]
-        if (sorted(df[col].unique()) == unique_range):
-            if (min(unique_range) == 1) | (min(unique_range) == 0):
-                tested.append(1)
-        #Captures all columns who's min value begins with 1 or 0 and all values are present from min() to max() in their unique values.
-        else:
-            tested.append(0)
-df
-print(tested)
+# for col in df.columns:
+#     try:
+#         if df[col].str.isnumeric().all() == False:
+#             labels.append(1)
+#             continue
+#         else:
+#             labels.append(0)
+#         #Captures all data with letters
+#     except:
+#         if df[col].dtype == float:
+#             labels.append(0)
+#             continue
+#         #Captures all  floats
+#         #HERE!
+#         values = df[col].unique()
+#         unique_range = [i for i in range(min(values), max(values)+1)]
+#         if (sorted(df[col].unique()) == unique_range):
+#             if (min(unique_range) == 1) | (min(unique_range) == 0):
+#                 labels.append(1)
+#         #Captures all columns who's min value begins with 1 or 0 and all values are present from min() to max() in their unique values.
+#             else:
+#                 labels.append(0)
+#         else:
+#                 labels.append(0)
 
-user_column_input = [0,]
-
-count = 0
-for x,y,z in zip(tested, user_column_input, df.columns):
-    if x == y:
-        print("Matched")
-    else:
-        print("Mislabeled", z, f"Labled {x} while column was {y}")
-        count += 1
-count
-
-df.PoolArea.unique()
+# count = 0
+# for x,y,z in zip(labels, user_column_input, df.columns):
+#     if x == y:
+#         print("Matched")
+#     else:
+#         print("Mislabeled", z, f"Labled {x} while column was {y}")
+#         count += 1
+# count
 
 
-values = df["PoolArea"].unique()
-unique_range = [i for i in range(min(values), max(values)+1)]
-if (sorted(df["PoolArea"].unique()) == unique_range):
-    if (min(unique_range) == 1) | (min(unique_range) == 0):
-        print("YAY! ", "PoolArea", "IS CATEGORICAL")
-else:
-    print("BOO")
+# sorted(df.DistanceFromHome.unique())
 
-unique_range
+
+
+
+
+
+# import seaborn as strings
+# from scipy.stats import norm
+# import matplotlib.pyplot as plt
+
+# sns.distplot(df.Parch, fit=norm
+# sns.distplot(df.Age)
+# sns.distplot(df.DistanceFromHome)
+# sns.distplot(df.YearsSinceLastPromotion)
+# sns.distplot(df.YearsWithCurrManager)
+# sns.distplot(df.Education.astype("category").cat.codes, fit=norm)
+
+# sns.distplot(df.Education, fit=norm)
+
+# df.DistanceFromHome.value_counts().idxmax() 
+# df.DistanceFromHome.median()
+# df.Age.max()
+
+# df.Age.describe()
+# df.DistanceFromHome.describe()
+
+# mislabeled.remove("PerformanceRating")
+# mislabeled.remove("EmployeeCount")
+# for col in mislabeled:
+#     sns.distplot(df[col], fit=norm)
+#     plt.show()
+
+
+
+# # Mean > Median > Mode = Right Skewed
+# # Mode > Median > Mean = Left Skewed
+
+# mean = df.YearsInCurrentRole.mean()
+# mode = df.YearsInCurrentRole.mode()
+# median = df.YearsInCurrentRole.median()
+
+# mean > median > mode
+
+# # If meets right skewed:
+
+# x = df.YearsSinceLastPromotion.value_counts().idxmax()
+# y = df.YearsSinceLastPromotion.max()
+# x
+# y
+
+# # If meets left skewed:
+
+# x = df.YearsSinceLastPromotion.value_counts().idxmax()
+# y = df.YearsSinceLastPromotion.min()
+
+
+
+
+
+# x = df.Education.value_counts().idxmax()
+# y = df.Education.max()
+
+# df.Education.unique()
+
