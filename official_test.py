541e0ba5ed212008dc07963fb381fae38126464d
symeonw <symeonwhite16@gmail.com>
2020-05-25 22:41:14 -0500

    data validation research

--- official_test.py
+++ official_test.py
@@ -35,64 +35,10 @@ print(end - start, " Stats")
 
 
 
-# #Continuous Testing
-# df = pd.read_csv("test_data/IBM_Data.csv")
-# column_dtypes = [0,1,1,0,1,0,1,1,0,0,1,1,0,1,1,1,1,1,0,0,0,1,1,0,1,1,0,1,0,0,1,0,0,0,0]
+test.dropped_cols_stats
+test.df.dropna(subset=["taxamount"],inplace=True)
 
-
-df.propertyzoningdesc.nunique()
-df.propertyzoningdesc.value_counts(normalize=True)
-df.rawcensustractandblock.isnull().sum()
-
-
-#TODO: Is there anything we could do to make the CI test faster ASAP?
-df = pd.read_csv("test_data/zillow_data.csv")
-
-def mean_confidence_interval(df:"two column dataframe", confidence=0.90):
-    """Takes in two columns: target(Category) and test column (continous)."""
-    target_len = len(df.iloc[:,1].unique())
-    ci = []
-    for i in range(target_len): #Accessing all unique categories in target, running all groupings of contious variables though test.
-        data = df[df.iloc[:,1] == df.iloc[:,1].unique()[i]].iloc[:,0]
-        if data < 30:
-            continue
-        a = 1.0 * np.array(data)
-        n = len(a)
-        m, se = np.mean(a), sem(a)
-        h = se * t.ppf((1 + confidence) / 2., n-1)
-        ci.append([m-h,m+h])
-    return ci
-
-start = time.time()
-x = mean_confidence_interval(df[["decktypeid", "taxamount"]])
-end = time.time()
-print(end - start, " Stats")
-
-df.decktypeid.nunique()
-
-df2=df[["rawcensustractandblock", "taxamount"]]
-
-df2.iloc[:,0].unique()
-df.decktypeid.unique()
-
-df = phase_one.df
-
-from scipy.stats import t
-a = np.array(data)
-t.interval(.9, len(a)-1, loc=np.mean(a))
-
-phase_one.df.censustractandblock.unique()
-
-df = pd.read_csv("test_data/IBM_Data.csv")
-
-df.EducationField.value_counts(normalized=True)
-
-x = pd.DataFrame(df.propertyzoningdesc.value_counts(normalize=True)>0.001).reset_index()
-x[x.iloc[:,1] == True].iloc[:,0]
-
-
-
-
+test.df
 
 
 
