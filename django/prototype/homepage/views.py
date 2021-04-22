6d6a7f84338b06cd8d79075488dfb26a7c2d36ea
Symeon White <54292200+Symeonw@users.noreply.github.com>
2021-04-22 09:50:24 -0500

    Delete django directory

--- django/prototype/homepage/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("<h1>TunaAI Homepage")
-
-def about(request):
-    return HttpResponse("<h1>TunaAI About Page")
